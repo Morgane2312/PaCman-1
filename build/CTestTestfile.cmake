799148c745f3d032654089ef1207aca531ad4caa
IT-Yohan <yohan.chapuis@gmail.com>
2024-03-14 06:25:12 +0100

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/workspace/paCman
+# Build directory: C:/workspace/paCman/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ main.c
@@ -0,0 +1,111 @@
+#include <SDL.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+#define PACMAN_SIZE 20
+#define PACMAN_SPEED 5
+
+typedef struct
+{
+    int x;
+    int y;
+} Position;
+
+int main(int argc, char *argv[])
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        fprintf(stderr, "Could not initialize SDL2: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "Pac-Man Game",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_SHOWN);
+
+    if (window == NULL)
+    {
+        fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL)
+    {
+        fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    Position pacman = {WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2};
+    bool running = true;
+
+    while (running)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+            else if (event.type == SDL_KEYDOWN)
+            {
+                switch (event.key.keysym.sym)
+                {
+                case SDLK_UP:
+                    pacman.y -= PACMAN_SPEED;
+                    break;
+                case SDLK_DOWN:
+                    pacman.y += PACMAN_SPEED;
+                    break;
+                case SDLK_LEFT:
+                    pacman.x -= PACMAN_SPEED;
+                    break;
+                case SDLK_RIGHT:
+                    pacman.x += PACMAN_SPEED;
+                    break;
+                }
+            }
+        }
+
+        // Teleport Pac-Man if it goes off the screen
+        if (pacman.x < 0)
+        {
+            pacman.x = WINDOW_WIDTH - PACMAN_SIZE;
+        }
+        else if (pacman.x > WINDOW_WIDTH - PACMAN_SIZE)
+        {
+            pacman.x = 0;
+        }
+        if (pacman.y < 0)
+        {
+            pacman.y = WINDOW_HEIGHT - PACMAN_SIZE;
+        }
+        else if (pacman.y > WINDOW_HEIGHT - PACMAN_SIZE)
+        {
+            pacman.y = 0;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+        SDL_Rect pacmanRect = {pacman.x, pacman.y, PACMAN_SIZE, PACMAN_SIZE};
+        SDL_RenderFillRect(renderer, &pacmanRect);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
